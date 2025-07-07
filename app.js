beb2f69844895923e612b4ddf1142eead99200fc
EvsEsneyderVega <evsdesarrollo@gmail.com>
2025-07-07 10:53:25 -0500

    404 add

--- app.js
+++ app.js
@@ -15,41 +15,69 @@ const PORT = process.env.PORT || 3000;
 //midlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/vista/css', express.static(path.join(__dirname, 'views', 'css')));
+app.use('/vista/js', express.static(path.join(__dirname, 'views', 'js')));
+app.use('/vista/images', express.static(path.join(__dirname, 'views', 'images')));
+
+//app.use(express.static('public'));
 
 //database conexion 
 const mysql = require('mysql2');
 
+const crearpass = (pass)=>{
+    const bcrypt = require('bcrypt');
+
+    bcrypt.hash(pass, 10, (err, hash) => {
+        if (err) throw err;
+        console.log('Hash generado:', hash);
+    });
+}
+
 app.get('/',(req,res)=>{
+    
     res.sendFile(path.join(__dirname,'views','login.html'));
+    //crearpass("algo");
 });
 
 // Vista para usuario Ingreso (nivel 1)
-app.get('/vista/ingreso', verificarToken, (req, res) => {
-    if (req.usuario.nivel != 1) {
-        return res.status(403).send('Acceso denegado');
-    }
+//app.get('/vista/ingreso', verificarToken, (req, res) => {
+
+app.get('/vista/ingreso', (req, res) => {
+    // if (req.usuario.nivel != 1) {
+    //     return res.status(403).send('Acceso denegado');
+    // }
     res.sendFile(path.join(__dirname, 'views', 'ingreso.html'));
 });
 
 // Vista para Calidad (nivel 2)
-app.get('/vista/calidad', verificarToken, (req, res) => {
-    if (req.usuario.nivel != 2) {
-        return res.status(403).send('Acceso denegado');
-    }
+//app.get('/vista/calidad', verificarToken, (req, res) => {
+
+app.get('/vista/calidad', (req, res) => {
+    // if (req.usuario.nivel != 2) {
+    //     return res.status(403).send('Acceso denegado');
+    // }
     res.sendFile(path.join(__dirname, 'views', 'calidad.html'));
 });
 
 // Vista para Admin (nivel 3)
-app.get('/vista/admin', verificarToken, (req, res) => {
-    if (req.usuario.nivel != 3) {
-        return res.status(403).send('Acceso denegado');
-    }
+//app.get('/vista/admin', verificarToken, (req, res) => {
+
+app.get('/vista/admin', (req, res) => {
+    // if (req.usuario.nivel != 3) {
+    //     return res.status(403).send('Acceso denegado');
+    // }
     res.sendFile(path.join(__dirname, 'views', 'admin.html'));
 });
 
-
 const rutasUsuario = require('./routes/usuario.routes.js');
+
 app.use('/api/usuarios',rutasUsuario);
+
+app.use((req,res)=>{
+     res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(PORT,()=>{
     console.log(`servidor en ${PORT}`)
